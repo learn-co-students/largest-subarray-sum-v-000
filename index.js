b76ef6244aa1acd548668cdeaefb73da09908fca
Mollie <mollie.stein@gmail.com>
2017-12-15 12:19:30 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+//Brute Force - O(n^2)
+
+// function largestSubarraySum(array) {
+// 	let largestSum = 0
+// 	let currentSum = 0
+
+// 	for (var i = 0; i < array.length; i++) {
+
+// 		for (var j = i; j < array.length; j++) {
+// 			currentSum += array[j]
+// 			if (currentSum > largestSum) {
+// 				largestSum = currentSum
+// 			}
+// 		}
+// 			currentSum = 0
+// 	}
+// 	return largestSum
+// }
+
+function largestSubarraySum(array) {
+}

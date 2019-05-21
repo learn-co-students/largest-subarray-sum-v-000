8e87a318b05fd01a6acf52802db04fdab54b26e8
rcrdlbl <bluh@outlook.com>
2019-05-21 00:12:41 -0400

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function largestSubarraySum(array) {
   let currentSum = 0
   let maxSum  = 0
   for (var i = 0; i < array.length; i++) {
-    array[i]
+    let currentNum = array[i]
+
+    currentSum = Math.max((currentNum + currentSum), 0)
+
+    maxSum = Math.max(currentSum, maxSum)
   }
+  return maxSum
 }
